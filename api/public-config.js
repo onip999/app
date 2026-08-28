e537764dfd6309937336a77b3953145f7500c16b
onip999 <giueppemaldera99@gmail.com>
2026-08-28 18:25:31 +0200

    Load operator identity from deployment settings

---
+++ api/public-config.js
@@ -0,0 +1,7 @@
+export default function handler(request, response) {
+  response.setHeader('Cache-Control', 'no-store')
+  response.status(200).json({
+    operatorName: process.env.OPERATOR_NAME || '',
+  })
+}
+
